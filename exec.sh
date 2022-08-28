9e2cc84cfcd12173e810053ddf32f54fa86b1cf3
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2022-08-29 01:02:25 +0530

    Added initial version of the scripts
    this commit includes a keyboard utility
    hover through the Readme file for more

---
+++ exec.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+path=$(pwd)
+dir="${PWD##*/}"
+status=`cat $path/status.txt`
+if [ $status == 0 ]
+then
+    xinput reattach 15 3
+    > $path/status.txt
+    echo 1 >> $path/status.txt
+    notify-send -t 3000 -i face-laughing "Keyboard enabled!"
+else
+    # xinput float 15
+    > $path/status.txt
+    echo 0 >> $path/status.txt
+   notify-send -t 3000 -i error "Keyboard disabled!"
+fi
