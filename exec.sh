cb30dffaa2ce02864cce85653529e15f5777a6e5
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2022-08-29 01:46:52 +0530

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -4,12 +4,12 @@ path=$(pwd)
 status=`cat $path/status.txt`
 if [ $status == 0 ]
 then
-    xinput reattach 15 3
+    xinput enable `your-device-id`
     > $path/status.txt
     echo 1 >> $path/status.txt
     notify-send -t 3000 -i face-laughing "Keyboard enabled!"
 else
-    xinput float 15
+    xinput disable `your-device-id`
     > $path/status.txt
     echo 0 >> $path/status.txt
    notify-send -t 3000 -i error "Keyboard disabled!"
